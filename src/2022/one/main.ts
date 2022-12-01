0f8ea32b2d24cf672d8367b51493469c5fde9794
Doug Skinner <dskinne12@gmail.com>
2022-12-01 14:58:36 -0500

    Complete day one

---
+++ src/2022/one/main.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+
+const main = async () => {
+    const input = await fs.readFileSync('src/2022/one/input.txt', 'utf-8');
+    const rows = input.split('\n');
+    let currentElfCalories = 0;
+    const elfCalories = [];
+
+    rows.forEach((row) => {
+        if (row === '') {
+            elfCalories.push(currentElfCalories);
+            currentElfCalories = 0;
+            return;
+        } else {
+            currentElfCalories += parseInt(row);
+        }
+    });
+
+    // Add the last elf's calories
+    elfCalories.push(currentElfCalories);
+
+    // find the biggest number
+    const maxCalories = Math.max(...elfCalories);
+
+    console.log(`Part one answer: ${maxCalories}`);
+
+    // find the top three
+    const topThree = elfCalories.sort((a, b) => b - a).slice(0, 3);
+
+    console.log(`Part two answer: ${topThree[0] + topThree[1] + topThree[2]}`);
+};
+
+main();
